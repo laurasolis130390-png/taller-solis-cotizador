e4367c52ba09b8ab10fb4fdade9fc027152e78bd
laurasolis130390-png <laura.solis130390@gmail.com>
2026-06-16 17:16:33 -0600

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_URL = "https://ubvhccnmgliioypwyxgy.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidmhjY25tZ2xpaW95cHd5eGd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMTYwNDAsImV4cCI6MjA5NjY5MjA0MH0.O8Hd4FSc1oWvqduJqO3JI767AOhy12bGL4I7Af8USs8";
 
 export const LOCAL_DEMO_USER = "solis";
 export const LOCAL_DEMO_PASSWORD = "cotizador";
